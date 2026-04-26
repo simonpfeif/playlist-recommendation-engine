2b3c876e00c84b2938dcbb55e2ca540f9c230037
Simon Pfeiffer <simonhpfeiffer@gmail.com>
2026-04-26 08:39:47 -0700

    API Gateway with GET route to API lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 # IAM role that Lambda assumes when it runs
@@ -31,23 +31,60 @@ resource "aws_iam_role" "lambda_role" {
 
 # Attach basic Lambda permissions (CloudWatch logging)
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
-    role       = aws_iam_role.lambda_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 # Zip up the lambda code
 data "archive_file" "lambda_zip" {
-    type = "zip"
-    source_file = "${path.module}/../lambda/api/handler.py"
-    output_path = "${path.module}/../lambda/api/handler.zip"
+  type        = "zip"
+  source_file = "${path.module}/../lambda/api/handler.py"
+  output_path = "${path.module}/../lambda/api/handler.zip"
 }
 
 # The Lambda function itself
 resource "aws_lambda_function" "api" {
-    filename = data.archive_file.lambda_zip.output_path
-    function_name = "playlist-recommendation-engine-api"
-    role = aws_iam_role.lambda_role.arn
-    handler = "handler.lambda_handler"
-    runtime = "python3.12"
-    source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "playlist-recommendation-engine-api"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "handler.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# API Gateway
+resource "aws_apigatewayv2_api" "gateway" {
+  name          = "playlist-recommendation-engine-api"
+  protocol_type = "HTTP"
+}
+
+# Connect API Gateway to Lambda
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.gateway.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.api.invoke_arn
+  payload_format_version = "2.0"
+}
+
+# Route GET requests to the Lambda
+resource "aws_apigatewayv2_route" "get" {
+  api_id    = aws_apigatewayv2_api.gateway.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+# Deploy the API
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.gateway.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Give API Gateway permission to invoke the Lambda
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.gateway.execution_arn}/*/*"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_name" {
 output "lambda_arn" {
     value = aws_lambda_function.api.arn
 }
+
+output "api_url" {
+  value = aws_apigatewayv2_stage.default.invoke_url
+}
